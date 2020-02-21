9fe4be4dc1945513404237e29e08cc2ce693f67b
barantaran <d.yurchev@yandex.ru>
2020-02-21 23:50:15 +0300

    Add messaging example

---
+++ Example/ConnectionClient.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+
+namespace UnityNATSNetwork
+{
+    public class ConnectionClient : Connection
+    {
+        public Queue<RemoteMessage> EventsReceived;
+
+        public static ConnectionClient S;
+
+        private new void Awake()
+        {
+            base.Awake();
+            EventsReceived = new Queue<RemoteMessage>();
+
+            if (S == null) S = this;
+            else Debug.LogError("Connection duplicate attempt");
+        }
+
+        private new void OnDestroy()
+        {
+            base.OnDestroy();
+        }
+
+        protected override void Subscribe()
+        {
+            Debug.Log("Subscribing on " + RemoteEventSubject);
+            NATSConnection.SubscribeAsync(RemoteEventSubject, (sender, args) =>
+            {
+                string stringReceived = Encoding.UTF8.GetString(args.Message.Data);
+                EventsReceived.Enqueue(JsonUtility.FromJson<RemoteMessage>(stringReceived));
+            });
+        }
+
+        public void SendCommand(RemoteMessage command)
+        {
+            Publish(command, RemoteCommandSubject);
+        }
+    }
+}
---
+++ Example/TestMessage.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityNATSNetwork;
+
+namespace UnityNATSNetwork
+{
+    public class TestMessage : MonoBehaviour
+    {
+        private int testID;
+ 
+        void Start()
+        {
+            testID = Random.Range(1, 10000);
+            RemoteMessage testEvent = new RemoteMessage((int)RemoteMessageCode.TestEvent, testID);
+            ConnectionClient.S.Publish(testEvent, ConnectionClient.S.RemoteEventSubject);
+        }
+
+        void Update()
+        {
+            if (ConnectionClient.S.EventsReceived.Count < 1) return;
+
+            RemoteMessage currentMessage = ConnectionClient.S.EventsReceived.Dequeue();
+            if(currentMessage.RemoteMessageCode == (int)RemoteMessageCode.TestEvent && currentMessage.ObjectID == testID)
+            {
+                Debug.Log("Test message received!");
+            }
+        }
+    }
+}
