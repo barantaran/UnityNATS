db2814a5e58fba787450766630dfa3ae3f89be13
barantaran <d.yurchev@yandex.ru>
2020-02-21 19:47:11 +0300

    Init commit

---
+++ UnityNATS/Connection.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using NATS.Client;
+using System.Text;
+
+namespace UnityNATSNetwork
+{
+    public abstract class Connection : MonoBehaviour
+    {
+        public string NATSURL = "nats://127.0.0.1:4222";
+        public string RemoteEventSubject = "Events";
+        public string RemoteCommandSubject = "Commands";
+
+        protected IConnection NATSConnection;
+
+        protected void Awake()
+        {
+            Connect();
+            Subscribe();
+        }
+
+        protected void OnDestroy()
+        {
+            NATSConnection.Close();
+        }
+
+        protected void OnDisable()
+        {
+            NATSConnection.Close();
+        }
+
+        private void Connect()
+        {
+            Debug.Log("Connectiong to NATS at " + NATSURL);
+
+            Options opts = ConnectionFactory.GetDefaultOptions();
+            opts.Url = NATSURL;
+
+            NATSConnection = new ConnectionFactory().CreateConnection(opts);
+        }
+
+        protected abstract void Subscribe();
+
+        public void Publish(RemoteMessage remoteMessage, string subject, bool flushImmediately = true)
+        {
+            string messageSerialized = JsonUtility.ToJson(remoteMessage);
+            byte[] messageEncoded = Encoding.UTF8.GetBytes(messageSerialized);
+
+            NATSConnection.Publish(subject, messageEncoded);
+
+            if (flushImmediately)
+            {
+                NATSConnection.Flush();
+            }
+
+            Debug.Log("Message sent " + messageSerialized + ", subject " + subject);
+        }
+    }
+}
---
+++ UnityNATS/RemoteMessage.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityNATSNetwork
+{
+    public enum RemoteMessageCode
+    {
+        TestCommand,
+        TestEvent
+    };
+
+    public struct RemoteMessage
+    {
+        public int RemoteMessageCode;
+        public int ObjectID;
+        public Vector3 ObjectPosition;
+        public Vector3 WaypointPosition;
+
+        public RemoteMessage(int messageCode, int objectID) : this()
+        {
+            RemoteMessageCode = messageCode;
+            ObjectID = objectID;
+        }
+    }
+}
